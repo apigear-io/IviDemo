fc1e29bb61f13eb0f44ff0ce498b5c670370ed2e
Jürgen Ryannel <juergen@ryannel.org>
2024-09-11 09:24:39 +0200

    init lab project

---
+++ IviLab/Source/IviLab/IviLab.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IviLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IviLab, "IviLab" );
